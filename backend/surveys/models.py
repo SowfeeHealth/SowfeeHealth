0b34f6e8e3d5eb0cb6bfdb8ece1144951c303c50
rrk0804 <rajitkhtr@gmail.com>
2025-02-07 11:20:22 -0500

    created model for storing survey responses

--- backend/surveys/models.py
+++ backend/surveys/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class SurveyResponse(models.Model):
+    studentName = models.CharField(max_length=250)
+    schoolEmail = models.EmailField(max_length=250)
+    q1 = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    q2 = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    q3 = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    q4 = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    q5 = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])

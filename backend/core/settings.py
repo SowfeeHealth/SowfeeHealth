e0d549bdf3728495b081b4ab09bda381b4d8fd5c
unknown <ziywang50@gmail.com>
2025-05-05 06:16:29 -0700

    action1 changed DJANGO_SECRET_KEY and added staticfiles settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -22,10 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)d8uh%dqyo=ll*=q6(-scvn_m!une_*bgx-ssk)1_x+k)x$yv4'
+# Change this hardcoded key to use environment variable
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-)d8uh%dqyo=ll*=q6(-scvn_m!une_*bgx-ssk)1_x+k)x$yv4')
+
+# Add your EC2 domain/IP to ALLOWED_HOSTS
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '*').split(',')]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Change this:
+# DEBUG = True
+
+# To this:
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ["*"]
 
@@ -151,6 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

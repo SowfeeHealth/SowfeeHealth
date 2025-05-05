1d79a1929906c56af58495b41f29b90ab3fc18f9
unknown <ziywang50@gmail.com>
2025-05-05 05:05:06 -0700

    action1

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR.parent / 'frontend' / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR.parent, 'frontend', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

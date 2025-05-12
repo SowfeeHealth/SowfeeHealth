8039641322d0b69933ee0085eda1cd8af9779246
unknown <ziywang50@gmail.com>
2025-05-12 09:06:18 -0700

    fix

--- mysql/create_health_user.sql
+++ mysql/create_health_user.sql
@@ -1,5 +1,13 @@
-﻿CREATE USER IF NOT EXISTS 'health_user'@'localhost' IDENTIFIED BY '$MYSQL_HEALTH_USER_PASSWORD';
-CREATE USER IF NOT EXISTS 'health_user'@'%' IDENTIFIED BY '$MYSQL_HEALTH_USER_PASSWORD';
-GRANT SELECT ON *.* TO 'health_user'@'localhost';
-GRANT SELECT ON *.* TO 'health_user'@'%';
+﻿-- 删除已存在的用户
+DROP USER IF EXISTS 'health_user'@'localhost';
+DROP USER IF EXISTS 'health_user'@'%';
+DROP USER IF EXISTS 'health_user'@'172.18.0.2';
+
+-- 创建新用户
+CREATE USER 'health_user'@'%' IDENTIFIED BY MYSQL_HEALTH_USER_PASSWORD;
+
+-- 授予更多权限
+GRANT ALL PRIVILEGES ON *.* TO 'health_user'@'%';
+
+-- 刷新权限
 FLUSH PRIVILEGES;

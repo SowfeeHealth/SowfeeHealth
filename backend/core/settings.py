b838e2e1f10ab1e8b8e3838ccc236842085ec7ee
unknown <ziywang50@gmail.com>
2025-05-12 10:01:39 -0700

    port change for Nginx

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -78,17 +78,15 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5500',
-]
-
 # 添加 CSRF 配置
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:8080',
-    'http://127.0.0.1:8080',
-    f'http://{os.getenv("EC2_HOST")}:8080',
+    'http://localhost',
+    'http://127.0.0.1',
+    f'http://{os.getenv("EC2_HOST")}',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'http://sowfeehealth.com',        
+    'http://www.sowfeehealth.com',       
 ]
 
 ROOT_URLCONF = 'core.urls'

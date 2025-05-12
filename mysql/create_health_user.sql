b1411b8684a6802106b2a7b3093090372df925da
unknown <ziywang50@gmail.com>
2025-05-12 09:29:58 -0700

    changed health check settings

--- mysql/create_health_user.sql
+++ mysql/create_health_user.sql
@@ -4,7 +4,7 @@ DROP USER IF EXISTS 'health_user'@'%';
 DROP USER IF EXISTS 'health_user'@'172.18.0.2';
 
 -- 创建新用户
-CREATE USER 'health_user'@'%' IDENTIFIED BY MYSQL_HEALTH_USER_PASSWORD;
+CREATE USER 'health_user'@'%' IDENTIFIED BY 'MYSQL_HEALTH_USER_PASSWORD';
 
 -- 授予更多权限
 GRANT ALL PRIVILEGES ON *.* TO 'health_user'@'%';

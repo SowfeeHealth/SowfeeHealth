2728e3307979b5ff1b15d5df0d6ca1387a8dc7b8
rrk0804 <rajitkhtr@gmail.com>
2025-05-13 22:30:15 -0400

    Improved code quality and structure for better comprehension

--- backend/surveys/urls.py
+++ backend/surveys/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("student-survey/", views.student_response_view),
-    path("flagged-students/", views.flagged_students_view),
-    path('survey/', views.survey_view, name='survey'),  # Survey view that maps survey page to send its responses
-    path('', views.index_view, name='index'),  # Root URL maps to the index_view
-    path('dashboard/<str:university>/', views.dashboard_view, name='dashboard'),
-    path('student/', views.students_view, name='student'),
+    path("api/student-responses/", views.student_response_view, name="student-responses-api"),
+    path("api/flagged-students/", views.flagged_students_view, name="flagged-students-api"),
+    path('api/students/', views.students_view, name='students-api'),
+    path('dashboard/<str:university_id>/', views.dashboard_view, name='dashboard'),
+    path('survey/', views.survey_view, name='survey'),
     path('login/', views.login_view, name='login'),
     path("register/", views.register_view, name="register"),
     path("logout/", views.logout_view, name="logout"),
+    path('', views.index_view, name='index'),
 ]

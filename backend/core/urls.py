05cd75aff46f788ea93d10ecdc5cbd330b0f7908
unknown <ziywang50@gmail.com>
2025-07-08 21:30:13 -0700

    added react serve urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('saodwmfiene*/', admin.site.urls, name="admin"),
     path("", include("surveys.urls")),
+    re_path(r'^react-static/(?P<path>.*)$', serve, {
+        'document_root': '/app/backend/staticfiles/react/static',
+    }),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

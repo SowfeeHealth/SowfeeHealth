dc56a2b16fad9c9c4581f1953c2cf27ad137edcc
ziywang50 <ziywang50@gmail.com>
2026-03-06 22:14:23 -0800

    added allowed hosts origin validator

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 import os
 import django
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
@@ -19,9 +20,11 @@ import chat.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": SessionAuthMiddleware(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
+    "websocket": AllowedHostsOriginValidator(
+        SessionAuthMiddleware(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
         )
     ),
 })
